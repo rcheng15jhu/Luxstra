dd3d974031a193e984a3f641122283abbe8ee632
Everett X <everettxu@Everetts-Air.fios-router.home>
2021-02-06 17:50:27 -0500

    Frontend aesthetics improvements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from "react";
 
-import {Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography} from "@material-ui/core";
+import {Button, Card, CardMedia, FormControl, InputLabel, List, MenuItem, Paper, Select, TextField, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -10,20 +10,40 @@ const useStyles = makeStyles((theme) => ({
     top: '0px',
     left: '0px',
     position: 'fixed',
+    backgroundColor: '#998866',
   },
-  origin: {
-    height: '100%',
-    width: '100%',
-    top: '0px',
-    left: '0px',
+  mainBox: {
+    height: '90%',
+    width: '90%',
+    top: '5%',
+    left: '4%',
     position: 'fixed',
+    backgroundColor: '#bbaa88',
+  },
+  selectionBox: {
+    top: '5%',
+    left: '5%',
+    height: '30%',
+    width: '20%',
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  directionsBox: {
+    top: '5%',
+    left: '30%',
+    height: '30%',
+    width: '65%',
+    position: 'absolute',
   },
   map: {
-    height: '100%',
-    width: '100%',
-    top: '0px',
-    left: '0px',
-    position: 'fixed',
+    top: '40%',
+    left: '5%',
+    height: '55%',
+    width: '90%',
+    position: 'absolute',
   },
 }));
 
@@ -48,26 +68,31 @@ function App() {
 
   return (
     <div className={classes.backgroundDiv}>
-      <div className={classes.selectionBox}>
-        <FormControl>
-          <InputLabel id="city">City</InputLabel>
-          <Select value={currentLocale} onChange={updateLocale}>
-            <MenuItem value={"Baltimore"}>Baltimore</MenuItem>
-            <MenuItem value={"Boston"}>Boston</MenuItem>
-            <MenuItem value={"WashingtonDC"}>Washington D.C.</MenuItem>
-          </Select>
-        </FormControl>
-        <form value={origin} onChange={updateOrigin}>
-          <TextField id="standard-basic" label="Origin" />
-        </form>
-        <form value={destination} onChange={updateDestination}>
-          <TextField id="standard-basic" label="Destination" />
-        </form>
-        <Button></Button>
-      </div>
-      <div className={classes.map}>
-
-      </div>
+      <Paper className={classes.mainBox} elevation={2}>
+        <Paper className={classes.selectionBox} elevation={1}>
+          <FormControl>
+            <InputLabel id="city">City</InputLabel>
+            <Select value={currentLocale} onChange={updateLocale}>
+              <MenuItem value={"Baltimore"}>Baltimore</MenuItem>
+              <MenuItem value={"Boston"}>Boston</MenuItem>
+              <MenuItem value={"WashingtonDC"}>Washington D.C.</MenuItem>
+            </Select>
+          </FormControl>
+          <form value={origin} onChange={updateOrigin}>
+            <TextField id="standard-basic" label="Origin" />
+          </form>
+          <form value={destination} onChange={updateDestination}>
+            <TextField id="standard-basic" label="Destination" />
+          </form>
+          <Button variant="contained">Create Routes</Button>
+        </Paper>
+        <Paper className={classes.directionsBox} elevation={1}>
+          <List style={{maxHeight: '100%', overflow: 'auto'}} />
+        </Paper>
+        <Card className={classes.map} elevation={1}>
+          <CardMedia src="https://www.google.com/maps/embed/v1/place?key=API_KEY&q=Space+Needle,Seattle+WA" />
+        </Card>
+      </Paper>
     </div>
   );
 }
