2097bf8e3947a1d6f2d85e9270b52360e4a80b3a
shintyl <tylerjshin@gmail.com>
2021-02-06 21:39:21 +0000

    ingest

---
+++ ingest/ingest.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const Client = require("@googlemaps/google-maps-services-js").Client;
+const pLimit = require('p-limit');
+
+const limit = pLimit(1);
+
+const client = new Client({})
+const key = process.env.GMAP_KEY
+
+const data = JSON.parse(fs.readFileSync('data.json', 'utf8').trim())
+
+async function getObjs() {
+    let promises = []
+    let result = []
+
+    for (let j = 1; j <= 10; j++) {
+        for (let i = 50 * (j - 1); i < 50 * j; i++) {
+        const feature = data.features[i]
+        const coords = feature.geometry.coordinates
+        promises.push(
+            limit(() => client
+                .nearestRoads({
+                    params: {
+                        points: [{ lat: coords[1], lng: coords[0] }],
+                        key: key
+                    }
+                })
+                .then(point => point.data.snappedPoints[0].placeId)
+                .then(placeId =>
+                    Promise.resolve(
+                        client.
+                            geocode({
+                                params: {
+                                    place_id: placeId,
+                                    key: key
+                                }
+                            })
+                            .catch((e) => {
+                                console.log(e.response.data.error_message);
+                            })
+                    )
+                )
+                .then(place => {
+                    result.push(
+                        {
+                            lat: coords[1],
+                            lng: coords[0],
+                            street: place.data.results[0].address_components[1].long_name,
+                            street_number: place.data.results[0].address_components[0].long_name
+                        })
+
+                })
+                .catch((e) => {
+                    console.log(e.response.data.error_message);
+                })
+        ))
+        }
+        await Promise.all(promises)
+    }
+    await Promise.all(promises)
+    return result
+}
+
+async function writeToOut() {
+    let out = { table: [] }
+    const objs = await getObjs()
+    out.table = objs
+    fs.writeFileSync('out.json', JSON.stringify(out, null, "\t"), 'utf8')
+}
+
+writeToOut()
