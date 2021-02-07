9b7e37659c9b0e308b69f8acd13917683624577d
Lawrence Mao <lawrencemao1@gmail.com>
2021-02-07 02:10:50 -0500

    background changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { Button, Card, CardMedia, FormControl, InputLabel, List, MenuItem, Paper, Select, TextField, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
+import background from "./resources/mainBackground3.png";
 
 const useStyles = makeStyles((theme) => ({
   backgroundDiv: {
@@ -11,7 +12,9 @@ const useStyles = makeStyles((theme) => ({
     top: '0px',
     left: '0px',
     position: 'fixed',
-    backgroundColor: '#998866',
+    backgroundImage: `url(${background})`,
+    //backgroundImage: 'linear-gradient(#FF8235, #30E8BF)',
+    backgroundSize: 'cover',
   },
   mainBox: {
     height: '90%',
@@ -19,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
     top: '5%',
     left: '4%',
     position: 'fixed',
-    backgroundColor: '#bbaa88',
+    backgroundColor: "#e6f2ff",
   },
   selectionBox: {
     top: '5%',
@@ -49,6 +52,9 @@ const useStyles = makeStyles((theme) => ({
     height: '55%',
     width: '90%',
     position: 'absolute',
+  },
+  routeButton: {
+    marginTop: '30px',
   },
 }));
 
@@ -89,7 +95,7 @@ function App(props) {
           <form value={destination} onChange={updateDestination}>
             <TextField id="standard-basic" label="Destination" />
           </form>
-          <Button variant="contained">Create Routes</Button>
+          <Button className={classes.routeButton} variant="contained">Create Routes</Button>
         </Paper>
         <Card className={classes.map} elevation={1}>
           <Map
